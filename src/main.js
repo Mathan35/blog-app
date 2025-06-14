2b8c75adbd6d7b06c76cdf25161cdd053962014a
mathan-ranium <mathan.kumar@ranium.in>
2025-06-14 21:52:05 +0530

    r

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from '@/App.vue';
 import router from '@/router'; // <--- ADD THIS LINE to import your router
 
 import { Amplify } from 'aws-amplify';
-import outputs from '@/aws-exports'; // Path to your generated outputs file
+import outputs from './aws-exports'; // Path to your generated outputs file
 
 Amplify.configure(outputs);
 
